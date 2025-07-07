6cc41170e6f1285ee5102340bd163225f1a521c9
MichaelMS23 <75376025+MichaelMS23@users.noreply.github.com>
2025-07-07 09:20:45 +0700

    Update AppDbContext.cs

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,11 +1,11 @@
 using Microsoft.EntityFrameworkCore;
-using TestApps.Models; // ✅ IMPORTANT!
+using TestApps.Models;
 
 namespace TestApps.Data
 {
     public class AppDbContext : DbContext
     {
-        public DbSet<Product> Products { get; set; } // ✅ This Product must match the using above
+        public DbSet<Product> Products { get; set; }
         public DbSet<User> Users { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
